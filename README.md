531543f5ab156675d276640fac13264bc92fc30e
noble0423 <ryan.noble82@gmail.com>
2018-02-21 20:40:01 -0800

    final save prior to HW turn-in.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# week-4-game
+# CRYSTALS GAME
+
+## Objective
+
+In this game, your objective is to make your score match the value that the computer is thinking.
+
+### Instructions
+
+* Reveal the computer's hidden value by clicking on the "?" button.
+
+* Each of the four crystals at the bottom of the page contains a hidden value that is randomly chosen each round.
+
+    * Click each crystal in order to add it's hidden value to the score panel.
+
+* Continue to increase your score by clicking more crystals.
+
+* Make your score match the computer's value and you win!
+
+    * Go over and you lose!
+
+* The game is automatically reset after each win or loss.
+
+    * The computer's value and all four crystals will all have new random values assigned to them. 
+
+
+
+
+* Hint: The only way to determine the hidden value of each crystal that you click is to evaluate the change in score.
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 
 // VARIABLES
 //=======================================================================================================
+
 var randomNum;
-// ... we generate a random number for ea crystal
 var randomGreen;
 var randomOrange;
 var randomPink;
@@ -10,26 +10,15 @@ var randomYellow;
 var score;
 var winCounter;
 var lossCounter;
-//var testNumber = 50;
 
 
 // FUNCTIONS
 //=======================================================================================================
-// Generate random computer pick
 
-// Generate random point value for ea crystal 
-//function generateRandomValue() {
-//    var numOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    
-//        for (i = 0; i < 4; i++) {
-//            numOptions = Math.floor(Math.random());  
-//            console.log(numOptions);
-        
-//        }
 // Computer picks Random number
 function computerRandomNumber () {
     // ... we generate a random number for computer
-    randomNum = Math.floor(Math.random() * 101) + 19;
+    randomNum = Math.floor(Math.random() * 102) + 19;
     console.log("Computer's random number is " + randomNum + ".");
     $("#random-button").on("click", function() {
         // ... and then dump the random number into our random-number div.
@@ -46,19 +35,19 @@ function greenCrystalRandomNumber () {
 }
 
 function orangeCrystalRandomNumber () {
-    // ... we generate a random number for green crystal
+    // ... we generate a random number for orange crystal
     randomOrange = Math.floor(Math.random() * 12) + 1;
     //console.log("Orange crystal equals " + randomOrange + ".");
 }
 
 function pinkCrystalRandomNumber () {
-    // ... we generate a random number for green crystal
+    // ... we generate a random number for pink crystal
     randomPink = Math.floor(Math.random() * 12) + 1;
     //console.log("Pink crystal equals " + randomPink + ".");
 }
 
 function yellowCrystalRandomNumber () {
-    // ... we generate a random number for green crystal
+    // ... we generate a random number for yellow crystal
     randomYellow = Math.floor(Math.random() * 12) + 1;
     //console.log("Yellow crystal equals " + randomYellow + ".");
 }
@@ -115,21 +104,6 @@ $(document).ready(function() {
 
     startGame();
 
-    // When random-button is clicked...
-    //$("#random-button").on("click", function() {
-        //console.log("random number is " + randomNum + ".");
-        // ... and then dump the random number into our random-number div.
-        //$("#random-number").text(randomNum);
-        //console.log("Random computer # is " + randomNum + ".");
-        //$("#random-number").text(randomNum);
-
-    //});
-
-    // When crystals are clicked...
-    //$(".crystal-image").on("click", function() {
-    //    console.log("button clicked");
-    //});
-
     // When green crystal is picked...
     $("#green-crystal").on("click", function() {
         // Assign var randomGreen to crystal
@@ -152,8 +126,6 @@ $(document).ready(function() {
             $("#lossCounter").text(lossCounter);
             resetGame();
         }
-        //var runningScore = score + randomGreen;
-        //console.log("total score = " + runningScore + ".");
         
     });
 
