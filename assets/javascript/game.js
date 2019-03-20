2a0473030b99c79b8d5226b031c01f53c6888b55
noble0423 <ryan.noble82@gmail.com>
2019-03-20 16:45:38 -0500

    code cleanup. removed all console logs from game.js. Added a missing <div class=col-lg-12> to the crystals area of index.html. deleted commented out CSS style. added line on game.js in order to chanfge the target score div back to '???' after a win or a loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 // VARIABLES
 //=======================================================================================================
 
@@ -17,63 +16,43 @@ var lossCounter;
 
 // Computer picks Random number
 function computerRandomNumber () {
-    // ... we generate a random number for computer
     randomNum = Math.floor(Math.random() * 102) + 19;
-    console.log("Computer's random number is " + randomNum + ".");
     $("#random-button").on("click", function() {
-        // ... and then dump the random number into our random-number div.
-        //console.log("Random computer # is " + randomNum + ".");
         $("#random-number").text(randomNum);
-
     });
 }
 
 function greenCrystalRandomNumber () {
-    // ... we generate a random number for green crystal
     randomGreen = Math.floor(Math.random() * 12) + 1;
-    //console.log("Green crystal equals " + randomGreen + ".");
 }
 
 function orangeCrystalRandomNumber () {
-    // ... we generate a random number for orange crystal
     randomOrange = Math.floor(Math.random() * 12) + 1;
-    //console.log("Orange crystal equals " + randomOrange + ".");
 }
 
 function pinkCrystalRandomNumber () {
-    // ... we generate a random number for pink crystal
     randomPink = Math.floor(Math.random() * 12) + 1;
-    //console.log("Pink crystal equals " + randomPink + ".");
 }
 
 function yellowCrystalRandomNumber () {
-    // ... we generate a random number for yellow crystal
     randomYellow = Math.floor(Math.random() * 12) + 1;
-    //console.log("Yellow crystal equals " + randomYellow + ".");
 }
 
 // Start Game brand new
 function startGame() {
     score = 0;
-    //console.log("score STARTS at " + score + ".");
     $("#score").text(score);
     
     winCounter = 0;
-    //console.log("win counter STARTS at " + winCounter + ".");
     $("#winCounter").text(winCounter);
     
     lossCounter = 0;
-    //console.log("loss counter STARTS at " + lossCounter + ".");
     $("#lossCounter").text(lossCounter);
     
     computerRandomNumber();
-
     greenCrystalRandomNumber();
-
     orangeCrystalRandomNumber();
-
     pinkCrystalRandomNumber();
-
     yellowCrystalRandomNumber();
 
 }
@@ -81,18 +60,14 @@ function startGame() {
 // Reset Game after a win or a loss
 function resetGame() {
     score = 0;
-    //console.log("score is RESET " + score + ".");
     $("#score").text(score);
-    //console.log("computer's # and all crystal's #'s have been RESET.")
+
+    $("#random-number").text("???");
 
     computerRandomNumber();
-
     greenCrystalRandomNumber();
-
     orangeCrystalRandomNumber();
-
     pinkCrystalRandomNumber();
-
     yellowCrystalRandomNumber();
 }
 
@@ -106,15 +81,11 @@ $(document).ready(function() {
 
     // When green crystal is picked...
     $("#green-crystal").on("click", function() {
-        // Assign var randomGreen to crystal
-        //console.log("green clicked and assigned " + randomGreen + ".");
         score += randomGreen;
-        //console.log("New score: " + score + ".");
         $("#score").text(score);
         if (score === randomNum) {
             alert("You win!");
             winCounter++;
-            //console.log("Win counter = " + winCounter + ".");
             $("#winCounter").text(winCounter);
             resetGame();
         }
@@ -122,7 +93,6 @@ $(document).ready(function() {
         else if (score >= randomNum) {
             alert("You Lose");
             lossCounter++;
-            //console.log("Loss counter = " + lossCounter + ".");
             $("#lossCounter").text(lossCounter);
             resetGame();
         }
@@ -131,15 +101,11 @@ $(document).ready(function() {
 
     // When orange crystal is picked...
     $("#orange-crystal").on("click", function() {
-        // Assign var randomOrange to crystal
-        //console.log("orange clicked and assigned " + randomOrange + ".");
         score += randomOrange;
-        //console.log("New score: " + score + ".");
         $("#score").text(score);
         if (score === randomNum) {
             alert("You win!");
             winCounter++;
-            //console.log("Win counter = " + winCounter + ".");
             $("#winCounter").text(winCounter); 
             resetGame();          
         }
@@ -147,7 +113,6 @@ $(document).ready(function() {
         else if (score >= randomNum) {
             alert("You Lose");
             lossCounter++;
-            //console.log("Loss counter = " + lossCounter + ".");
             $("#lossCounter").text(lossCounter);
             resetGame();
         }
@@ -156,15 +121,11 @@ $(document).ready(function() {
 
     // When pink crystal is picked...
     $("#pink-crystal").on("click", function() {
-        // Assign var randomPink to crystal
-        //console.log("pink clicked and assigned " + randomPink + ".");
         score += randomPink;
-        //console.log("New score: " + score + ".");
         $("#score").text(score);
         if (score === randomNum) {
             alert("You win!");
             winCounter++;
-            //console.log("Win counter = " + winCounter + ".");
             $("#winCounter").text(winCounter);
             resetGame();
         }
@@ -172,7 +133,6 @@ $(document).ready(function() {
         else if (score >= randomNum) {
             alert("You Lose");
             lossCounter++;
-            //console.log("Loss counter = " + lossCounter + ".");
             $("#lossCounter").text(lossCounter);
             resetGame();
         }
@@ -181,15 +141,11 @@ $(document).ready(function() {
 
     // When yellow crystal is picked...
     $("#yellow-crystal").on("click", function() {
-        // Assign var randomYellow to crystal
-        //console.log("yellow clicked and assigned " + randomYellow + ".");
         score += randomYellow;
-        //console.log("New score: " + score + ".");
         $("#score").text(score);
         if (score === randomNum) {
             alert("You win!");
             winCounter++;
-            //console.log("Win counter = " + winCounter + ".");
             $("#winCounter").text(winCounter);
             resetGame();
         }
@@ -197,7 +153,6 @@ $(document).ready(function() {
         else if (score >= randomNum) {
             alert("You Lose");
             lossCounter++;
-            //console.log("Loss counter = " + lossCounter + ".");
             $("#lossCounter").text(lossCounter);
             resetGame();
         }
